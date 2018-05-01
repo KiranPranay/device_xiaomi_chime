1688d9bcaed0e062229b038f84a764a0707d188f
Jesse Chan <jc@lineageos.org>
2018-04-30 23:02:55 -0700

    chime: Enable option for full screen aspect ratio
    Change-Id: I2a1b2acc5ed97ccb6795dc371c2b3ac9cb4f9d03
    Signed-off-by: HeroBuxx <herobuxx@conqueros.co>

--- device.mk
+++ device.mk
@@ -13,6 +13,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
