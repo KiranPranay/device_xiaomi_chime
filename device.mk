89b7958ef02c4a1502d1ae89abbdcff0b23cde76
Bowgo Tsai <bowgotsai@google.com>
2019-04-08 18:28:48 +0800

    chime: Adding GSI keys
    Bug: 112293933
    Test: tree hugger
    Change-Id: I112ece91566e6cea066cd637bfed5cae6761ae66
    Signed-off-by: HeroBuxx <herobuxx@conqueros.co>

--- device.mk
+++ device.mk
@@ -7,6 +7,9 @@
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
