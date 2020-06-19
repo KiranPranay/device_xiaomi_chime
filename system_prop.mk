bff03feec3c7275eaa50e39add7ca87d861ae75e
acras01 <acras1@gmail.com>
2020-06-19 16:49:40 +0300

    juice: prop: Add additional bt audio props (aptx, LDAC)
    Signed-off-by: kiranpranay <pranaybannu12@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -1,10 +1,12 @@
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
+    persist.bt.enableAptXHD=true \
     persist.vendor.bt.a2dp.aac_whitelist=false \
     persist.vendor.btstack.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aptxadaptive-aac-ldac \
     persist.vendor.btstack.enable.splita2dp=true \
     persist.vendor.btstack.enable.twsplus=true \
     persist.vendor.btstack.enable.twsplussho=true \
+    persist.service.btui.use_aptx=1 \
     ro.bluetooth.library_name=libbluetooth_qti.so \
     vendor.bluetooth.soc=cherokee
 
