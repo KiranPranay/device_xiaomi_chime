30035df573db74bad2fe119a911288eac0e4ada7
Joboy Jhem Francisco <joboyjhem24@gmail.com>
2021-04-15 12:30:01 +0800

    juice: add redfin April 2021 fingerprint

--- derp_juice.mk
+++ derp_juice.mk
@@ -19,3 +19,5 @@ PRODUCT_MODEL := sm6115
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys"
