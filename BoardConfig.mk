696d2a4be6e49dd67591cff7b753b4bc9564d1f7
Joboy Jhem Francisco <joboyjhem24@gmail.com>
2021-04-15 15:38:35 +0800

    juice: target provides qti telephony jar

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -148,6 +148,9 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.default
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TARGET_USERIMAGES_USE_F2FS := true
 
+# RIL
+TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
+
 # Sepolicy
 include device/qcom/sepolicy/SEPolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
