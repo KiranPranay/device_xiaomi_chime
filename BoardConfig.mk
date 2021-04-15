ffe066bc77346bf7aa3c2440801ca98b949a4e7b
Joboy Jhem Francisco <joboyjhem24@gmail.com>
2021-04-15 17:20:37 +0800

    juice: generate config fs
    I totally forgot about this line, fixes bootloop.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -62,6 +62,9 @@ TARGET_USES_HWC2 := true
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
 
+# Filesystem
+TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
+
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_juice
 TARGET_RECOVERY_DEVICE_MODULES := libinit_juice
