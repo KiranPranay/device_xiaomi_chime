26aae552b2ec2b92b54a03e8a36349eb2154ab27
Joboy Jhem Francisco <joboyjhem24@gmail.com>
2021-04-16 14:46:45 +0800

    juice: add support for DT2W

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -147,6 +147,7 @@ BUILD_WITHOUT_VENDOR := true
 
 # Power
 TARGET_USES_INTERACTION_BOOST := true
+TARGET_TAP_TO_WAKE_NODE := "/dev/input/event3"
 
 # Platform
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno610
