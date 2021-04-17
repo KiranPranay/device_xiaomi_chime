de5100e4cd8e4f38b8cb73cc9805bc8ebdba7148
Joboy Jhem Francisco <joboyjhem24@gmail.com>
2021-04-17 09:58:53 +0800

    juice: build health

--- device.mk
+++ device.mk
@@ -73,6 +73,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.default:$(TARGET_COPY_OUT_RAMDISK)/fstab.default
 
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
+
 # Init
 PRODUCT_PACKAGES += \
     init.mi_thermald.rc
