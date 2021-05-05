c544f6ad79f91fa52a6ba7df50c6b2e486f0014c
Kuba Wojciechowski <nullbytepl@gmail.com>
2021-05-05 19:34:08 +0800

    chime: Disable kpti
    It can also help increase performance by a lot in some scenarios.
    
    Signed-off-by: Kuba Wojciechowski <nullbytepl@gmail.com>
    Change-Id: Ica465f586a44129047c266597316045fab762550
    Signed-off-by: frost. (frostg-012) <frost@nusantararom.org>
    Signed-off-by: HeroBuxx <herobuxx@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,6 +47,7 @@ BOARD_KERNEL_CMDLINE += swiotlb=2048
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += androidboot.fstab_suffix=default
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
+BOARD_KERNEL_CMDLINE += kpti=off
 
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
