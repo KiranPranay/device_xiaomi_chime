342b2fb5f6e3c5246134b02e14b8f6ad5302f47b
ѕαмαя νιѕρυтє <samarvispute121@gmail.com>
2021-05-07 07:38:16 +0800

    juice: add memory optimization props

--- system_prop.mk
+++ system_prop.mk
@@ -1,3 +1,11 @@
+# Memory optimizations
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.qti.sys.fw.bservice_enable=true \
+    ro.sys.fw.bservice_limit=5 \
+    ro.sys.fw.bservice_age=5000 \
+    ro.am.reschedule_service=true \
+    ro.sys.fw.bg_apps_limit=24
+
 # Zygote
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.device_config.runtime_native.usap_pool_enabled=true
