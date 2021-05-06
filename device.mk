0b35c16fc00bb406fbe1e41ee8b3099d3fbb942d
Volodymyr Zhdanov <wight554@gmail.com>
2021-05-07 07:53:08 +0800

    juice: add bt_stack_qti.conf to soong namespaces

--- device.mk
+++ device.mk
@@ -20,7 +20,9 @@ $(call inherit-product-if-exists, vendor/xiaomi/juice/juice-vendor.mk)
 
 # Soong Namespace
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    vendor/qcom/opensource/commonsys/packages/apps/Bluetooth \
+    vendor/qcom/opensource/commonsys/system/bt/conf
     
 PRODUCT_BUILD_SUPER_PARTITION := false
 BOARD_BUILD_PRODUCT_IMAGE := true
