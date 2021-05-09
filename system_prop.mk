538f3c5e70954a8e1e1beac98a8844cb4584e9ac
Joboy Jhem Francisco <joboyjhem24@gmail.com>
2021-05-09 09:13:46 +0800

    juice: enable zram-writeback job

--- system_prop.mk
+++ system_prop.mk
@@ -6,6 +6,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.am.reschedule_service=true \
     ro.sys.fw.bg_apps_limit=24
 
+# ZRAM
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.zram.mark_idle_delay_mins=60 \
+    ro.zram.first_wb_delay_mins=180 \
+    ro.zram.periodic_wb_delay_hours=24
+
 # Zygote
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.device_config.runtime_native.usap_pool_enabled=true
