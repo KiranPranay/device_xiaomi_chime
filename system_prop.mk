9134c75d56bf2f5a9939d686a831b7ccf46baa03
Zilberto Junior <zjunior06@pixelexperience.org>
2021-05-09 09:18:03 +0800

    juice: Add some props for better ram management

--- system_prop.mk
+++ system_prop.mk
@@ -4,7 +4,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.sys.fw.bservice_limit=5 \
     ro.sys.fw.bservice_age=5000 \
     ro.am.reschedule_service=true \
-    ro.sys.fw.bg_apps_limit=24
+    ro.sys.fw.bg_apps_limit=32 \
+    ro.config.fha_enable=true \
+    ro.config.dha_cached_max=16 \
+    ro.config.dha_empty_max=42 \
+    ro.config.dha_empty_init=32 \
+    ro.config.dha_lmk_scale=0.545 \
+    ro.config.dha_th_rate=2.3 \
+    ro.config.sdha_apps_bg_max=64 \
+    ro.config.sdha_apps_bg_min=8
 
 # ZRAM
 PRODUCT_PROPERTY_OVERRIDES += \
