91c99396e0c26f7fd3d1fffc82c0c4fb04bdcd16
I-n-o-k <inok.dr189@gmail.com>
2021-06-15 01:54:06 +0000

    juice: Add prebuilt GCamGO
    Signed-off-by: kiranpranay <pranaybannu12@gmail.com>

--- device.mk
+++ device.mk
@@ -116,6 +116,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
 
+# Google Camera
+PRODUCT_PACKAGES += \
+    GoogleCameraGo
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
