ba2f60b24064ec15ef980e417308424007522c41
I-n-o-k <inok.dr189@gmail.com>
2021-06-15 01:54:06 +0000

    juice: Add prebuilt GCamGO
    Signed-off-by: kiranpranay <pranaybannu12@gmail.com>

--- device.mk
+++ device.mk
@@ -56,6 +56,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     BluetoothQti
 
+# Google Camera
+PRODUCT_PACKAGES += \
+    GoogleCameraGo
+
 # Display
 PRODUCT_PACKAGES += \
     libdisplayconfig \
