6552221da77728a07bfb7694edf233d3fa81c2a9
GtrCraft <nickvanbokhorst@hotmail.com>
2021-07-09 21:45:04 +0800

    juice: Override VoLTE props
    - Force Enable VoLTE
    - Fix Wifi Calling

--- system_prop.mk
+++ system_prop.mk
@@ -8,6 +8,16 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.bluetooth.library_name=libbluetooth_qti.so \
     vendor.bluetooth.soc=cherokee
 
+# IMS
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.ims_volte_enable=1 \
+    persist.dbg.vt_avail_ovr=1 \
+    persist.dbg.wfc_avail_ovr=1 \
+    persist.vendor.radio.force_ltd_sys_ind=1 \
+    persist.vendor.radio.voice_on_lte=1 \
+    persist.vendor.radio.calls.on.ims=1
+
 # Memory optimizations
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.qti.sys.fw.bservice_enable=true \
