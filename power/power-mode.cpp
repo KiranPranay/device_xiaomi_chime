d8c82b86af32c2a604777744db97f34167ce3c02
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-07-09 13:08:32 +0700

    juice: power: Implement extension for LOW_POWER mode
     * for our kernelspace battery saver
     * inspired from https://github.com/LineageOS/android_device_xiaomi_sdm845-common/commit/8af17a83d41388c9fe3d4e550b702134293b40e4
    
    Signed-off-by: kiranpranay <pranaybannu12@gmail.com>

--- power/power-mode.cpp
+++ power/power-mode.cpp
@@ -15,6 +15,7 @@
  */
 
 #include <aidl/android/hardware/power/BnPower.h>
+#include "power-common.h"
 #include <android-base/file.h>
 #include <android-base/logging.h>
 #include <linux/input.h>
@@ -76,6 +77,7 @@ using ::aidl::android::hardware::power::Mode;
 bool isDeviceSpecificModeSupported(Mode type, bool* _aidl_return) {
 	switch (type) {
 		case Mode::DOUBLE_TAP_TO_WAKE:
+                case mode::LOW_POWER:
 			*_aidl_return = true;
 			return true;
 		default:
@@ -85,7 +87,9 @@ bool isDeviceSpecificModeSupported(Mode type, bool* _aidl_return) {
 
 bool setDeviceSpecificMode(Mode type, bool enabled) {
 	switch (type) {
-		case Mode::DOUBLE_TAP_TO_WAKE: {
+		case Mode::DOUBLE_TAP_TO_WAKE: 
+                case Mode::LOW_POWER:
+                    power_hint(POWER_HINT_LOW_POWER, reinterpret_cast<void*>(enabled));
 			int fd = open_ts_input();
 			if (fd == -1) {
 				LOG(WARNING)
