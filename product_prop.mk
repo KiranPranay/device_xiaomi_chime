5ab05315c5bcadf56d07ec147cd0a869846e68b5
Alex Winkowski <dereference23@outlook.com>
2021-08-07 18:18:01 +0530

    juice: Enable IORap app launch prefetching
    Commit b73d5516136d1a4de39afd74f564daf70b946889 in build/make (rvc-qpr2)
    - disabled IORap by default due to a "regression" without mentioning what
    the regression is. Considering how it's been working fine for the past 6
    months and has demonstrated improvements to app launch time [1], let's
    re-enable it here.
    
    - Enable iorapd tracing with perfetto
       App startups will now have perfetto-based tracing from iorapd.
    
    - Enable readahead for iorapd.
    
    Signed-off-by: Alex Winkowski <dereference23@outlook.com>
    Signed-off-by: kiranpranay <pranaybannu12@gmail.com>

--- product_prop.mk
+++ product_prop.mk
@@ -20,24 +20,30 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     debug.hwui.renderer=opengl \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
-    debug.sf.latch_unsignaled=0
+    debug.sf.latch_unsignaled=0 \
     ro.hardware.egl=adreno \
     ro.hardware.vulkan=adreno \
     ro.surface_flinger.protected_contents=true \
     ro.surface_flinger.has_HDR_display=true \
-    ro.surface_flinger.use_color_management=true
-    debug.mdpcomp.logs=0
-    vendor.gralloc.disable_ubwc=0
-    vendor.display.disable_scaler=0
-    vendor.display.disable_excl_rect=0
-    vendor.display.disable_excl_rect_partial_fb=1
-    vendor.display.comp_mask=0
-    debug.sf.disable_client_composition_cache=1
-    vendor.display.disable_rotator_ubwc=1
-    vendor.display.disable_layer_stitch=0
-    vendor.display.disable_hw_recovery_dump=1
-    debug.sf.hw=0
+    ro.surface_flinger.use_color_management=true \
+    debug.mdpcomp.logs=0 \
+    vendor.gralloc.disable_ubwc=0 \
+    vendor.display.disable_scaler=0 \
+    vendor.display.disable_excl_rect=0 \
+    vendor.display.disable_excl_rect_partial_fb=1 \
+    vendor.display.comp_mask=0 \
+    debug.sf.disable_client_composition_cache=1 \
+    vendor.display.disable_rotator_ubwc=1 \
+    vendor.display.disable_layer_stitch=0 \
+    vendor.display.disable_hw_recovery_dump=1 \
+    debug.sf.hw=0 \
     debug.egl.hw=0
+
+# IORap
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.iorapd.enable=true \
+    iorapd.perfetto.enable=true \
+    iorapd.readahead.enable=true
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
