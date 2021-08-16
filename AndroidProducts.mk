f2ddae55a5c3f2e3c10ee8619d532e12768e4a0f
kiranpranay <pranaybannu12@gmail.com>
2021-08-16 17:04:53 +0530

    Juice: PEX BringUp
    Signed-off-by: kiranpranay <pranaybannu12@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_juice.mk
+    $(LOCAL_DIR)/aosp_juice.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_juice-user \
-    derp_juice-userdebug \
-    derp_juice-eng
+    aosp_juice-user \
+    aosp_juice-userdebug \
+    aosp_juice-eng
---
+++ aosp_juice.mk
@@ -0,0 +1,43 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, device/xiaomi/juice/device.mk)
+
+# Inherit some common Aosp stuff.
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_GAPPS_ARCH := arm64
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_PIXEL_CHARGER := true
+WITH_GAPPS := true
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := aosp_juice
+PRODUCT_DEVICE := juice
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Juice
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# PEX Official
+PEX_BUILD_TYPE := OFFICIAL
+
+# PEX Maintainer
+PEX_MAINTAINER := PranayKiran
+
+# Boot Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Fingerprint
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys"
+BUILD_DESCRIPTION := redfin-user 11 RQ3A.210805.001.A1 7474174 release-keys
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="$(BUILD_DESCRIPTION)" 
