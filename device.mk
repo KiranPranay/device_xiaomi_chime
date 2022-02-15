4a2ed30f21485860595371b3441668a41b05aa2f
Robert Shih <robertshih@google.com>
2022-02-15 17:07:30 +0000

    chime: enable AIDL DRM HALs
    Bug: 219538389
    Test: atest GtsMediaTestCases
    Change-Id: I431554dcbef014f8235f048ee062a218a2131f9c
    Signed-off-by: HeroBuxx <me@buxxed.tk>
    Signed-off-by: kiranpranay <pranaybannu12@gmail.com>

--- device.mk
+++ device.mk
@@ -150,7 +150,7 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm-service.clearkey
 
 # FM
 PRODUCT_PACKAGES += \
