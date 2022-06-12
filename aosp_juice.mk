991d38ac5ad48f67d4622ef5d9d915b008268a58
kiranpranay <pranaybannu12@gmail.com>
2022-06-12 14:00:34 +0530

    juice: update june Raven FP
    Signed-off-by: kiranpranay <pranaybannu12@gmail.com>

--- aosp_juice.mk
+++ aosp_juice.mk
@@ -36,8 +36,8 @@ TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Fingerprint
-BUILD_FINGERPRINT := "google/raven/raven:12/SP2A.220505.002/8353555:user/release-keys"
-BUILD_DESCRIPTION := raven-user 12 SP2A.220505.002 8353555 release-keys
+BUILD_FINGERPRINT := "google/raven/raven:12/SQ3A.220605.009.B1/8650216:user/release-keys"
+BUILD_DESCRIPTION := raven-user 12 SQ3A.220605.009.B1 8650216 release-keys
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
