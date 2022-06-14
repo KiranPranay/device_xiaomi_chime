ae92ef836fea13ac7bd5cd2e6aa8e234b3e19822
Pulkit077 <pulkitagarwal2k1@gmail.com>
2022-06-14 18:13:45 +0000

    chime: Uprev DRM to 1.4
    * Also fix manifest
    
    Signed-off-by: HeroBuxx <herobuxx@gmail.com>

--- device.mk
+++ device.mk
@@ -155,9 +155,7 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0.vendor \
-    android.hardware.drm@1.2.vendor \
-    android.hardware.drm@1.3.vendor \
+    android.hardware.drm@1.4.vendor \
     android.hardware.drm@1.4-service.clearkey
 
 # Gatekeeper
