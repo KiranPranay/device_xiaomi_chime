70d81c7012b0d441554ff1b205d6d40849ba786e
HeroBuxx <herobuxx@gmail.com>
2022-06-17 14:44:08 +0000

    chime: Remove LineageOS features
    Signed-off-by: HeroBuxx <herobuxx@gmail.com>
    Signed-off-by: kiranpranay <pranaybannu12@gmail.com>

--- device.mk
+++ device.mk
@@ -22,8 +22,7 @@ PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-im
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
@@ -221,10 +220,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     lights.bengal
-
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
 
 # Keylayout
 PRODUCT_COPY_FILES += \
@@ -463,10 +458,6 @@ PRODUCT_PACKAGES += \
 # Touchscreen
 PRODUCT_PACKAGES += \
     libtinyxml2
-
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
 
 # USB
 PRODUCT_PACKAGES += \
