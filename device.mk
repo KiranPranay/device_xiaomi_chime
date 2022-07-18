188baff6195f6f34a3b7b02624f0ce5a8c8af1b3
HeroBuxx <herobuxx@gmail.com>
2022-07-18 10:39:16 +0000

    chime: Remove snap
    * R.I.P LineageOS Snap
    
    Signed-off-by: HeroBuxx <herobuxx@gmail.com>

--- device.mk
+++ device.mk
@@ -116,9 +116,6 @@ PRODUCT_PACKAGES += \
     libgui_vendor \
     libxml2
 
-PRODUCT_PACKAGES += \
-    Snap
-
 # Component overrides
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
