fafd8a59d76c28530f93f3caa3048e2015cf50c9
kiranpranay <pranaybannu12@gmail.com>
2022-07-27 21:30:03 +0530

    chime: remove packages on build time
    * Also, remove Snap/Camera2
    
    Signed-off-by: kiranpranay <pranaybannu12@gmail.com>

--- device.mk
+++ device.mk
@@ -390,6 +390,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.class_main.sh \
